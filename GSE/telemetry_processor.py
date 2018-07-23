3f4db9c6452d77d15a71c57732a1abe0a323427e
Kyle Kung <41412788+krkung@users.noreply.github.com>
2018-07-22 18:05:54 -0700

    Update telemetry_processor.py

--- GSE/telemetry_processor.py
+++ GSE/telemetry_processor.py
@@ -2,23 +2,49 @@ import csv
 import glob
 
 class TelemetryProcessor:
+    
+    #Global variable defining all data over time
+    data_dict = []
+    
     """
     Loads in any calibration files or csv map (of the order of values in input list)
     """
     def __init__(packetMap):
         #Selects csv file with unknown file name
-        data = glob.glob('*.csv')
-        return data
-            
+        packet = glob.glob('*.csv')
+        return packet
+        
+    """
+    Adds limit status to each parameter in dictionary
+    """
+    def addLimitStatus(data):
+        limits = []
+        if len(data) == 0:
+            pass
+        else:
+            for dictionary in data:
+                count = 0
+                for key, value in dictionary.items():
+                    if value >= limit[count]:
+                        count += 1
+                        return value + ', Limit Exceeded'
+                    elif value < limit[count]:
+                        count += 1
+                        return value + ', Within Limit'
+                    else:
+                        count += 1
+                        return value + ', Error'
+                        
     """
     Takes in a list of data in specific known order,
-    outputs dict of the form ["name", calibrated value, limit status)]
+    outputs list of dict of the form [{("first_data_set", calibrated value), ....}, \
+    {"second_data_set", calibrated value), ....}]
     """
     def processPacket(packet):
         #Reads csv and places into a dictionary
-        with open(data, newline = '') as csvfile:
-            data_dict = csv.DictReader(csvfile)
-            for row in csvfile:
+        with open(packet, newline = '') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
                 row['Time_Start'],row['Flight State'],row['exp time'],row['Altitude'], \
                 row['Velocity_X'],row['Velocity_Y'],row['Velocity_Z'],row['Attitude_X'],row['Attitude_Y'],row['Attitude_Z'], \
                 row['Acceleration_X'],row['Acceleration_Y'],row['Acceleration_Z'],row['Ang_Vel_X'],row['Ang_Vel_Y'],row['Ang_Vel_Z'], \
@@ -26,9 +52,16 @@ class TelemetryProcessor:
                 row['Frequency Measure'],row['Magnetometer'],row['Current'],row['Voltage'],row['Timer'], \
                 row['BD_Acceleration_X'],row['BD_Acceleration_Y'],row['BD_Acceleration_Z'], \
                 row['BD_Ang_Vel_X'],row['BD_Ang_Vel_Y'],row['BD_Ang_Vel_Z'],row['MOSFET'],row['Time_End']
+                
+                data_dict.append(row)
             #Note: Add Blue Dawn Errors to dictionary if they end up in CSV
-            #Last 3 rows report Blue Dawn data. Order may need to be changed depending on final CSV data order.
-            #Question: How often is the CSV updated? This model assumes all data at a given time is reported in one row of the CSV 
-            limits = []
-            for i 
+            '''
+            Last 3 rows report Blue Dawn data. Order may need to be changed depending on final CSV data order. Written this
+            way so that a list of dictionaries is created. Each dictionary represents all data at a given point in time
+            '''
+            #Question: How often is the CSV updated? This model assumes all data at a given time is reported in one row of the CSV
+            return data_dict
+            
+
+            
             
