589295d0c8003758f1eb9862d2d964c5bf8d8b20
Kyle Kung <41412788+krkung@users.noreply.github.com>
2018-07-22 18:54:35 -0700

    Update telemetry_processor.py

--- GSE/telemetry_processor.py
+++ GSE/telemetry_processor.py
@@ -18,22 +18,26 @@ class TelemetryProcessor:
     Adds limit status to each parameter in dictionary
     """
     def addLimitStatus(data):
-        limits = []
+        limits = [1e7, 0, 1e7, 333000, 3500, 3500, 3500, 1000, 1000, 1000, 5, 5, 5, \
+                  1000, 1000, 1000, 1, 1, 1, 1, 1, 1, 100, 100, 10000, 10000, 1e7, \
+                  5, 5, 5, 1000, 1000, 1000, 0, 1e7]
         if len(data) == 0:
             pass
         else:
             for dictionary in data:
                 count = 0
                 for key, value in dictionary.items():
-                    if value >= limit[count]:
+                    if count == 1 or count == 33:   #For 'Flight State' Index
+                        pass
+                    elif float(value) >= limit[count]:
                         count += 1
                         return value + ', Limit Exceeded'
-                    elif value < limit[count]:
+                    elif float(value) < limit[count]:
                         count += 1
                         return value + ', Within Limit'
                     else:
                         count += 1
-                        return value + ', Error'
+                        pass
                         
     """
     Takes in a list of data in specific known order,
@@ -52,16 +56,14 @@ class TelemetryProcessor:
                 row['Frequency Measure'],row['Magnetometer'],row['Current'],row['Voltage'],row['Timer'], \
                 row['BD_Acceleration_X'],row['BD_Acceleration_Y'],row['BD_Acceleration_Z'], \
                 row['BD_Ang_Vel_X'],row['BD_Ang_Vel_Y'],row['BD_Ang_Vel_Z'],row['MOSFET'],row['Time_End']
-                
-                data_dict.append(row)
-            #Note: Add Blue Dawn Errors to dictionary if they end up in CSV
+            #Note: Add Blue Dawn Errors to dictionary if they become part of csv
             '''
             Last 3 rows report Blue Dawn data. Order may need to be changed depending on final CSV data order. Written this
             way so that a list of dictionaries is created. Each dictionary represents all data at a given point in time
             '''
             #Question: How often is the CSV updated? This model assumes all data at a given time is reported in one row of the CSV
+                data_dict.append(row)
+                
+            data_dict = addLimitStatus(data_dict)
             return data_dict
-            
-
-            
-            
+           
