2c1ee65397b8abe21bced6e07b8efdeb9135e2c8
Kyle Kung <41412788+krkung@users.noreply.github.com>
2018-07-25 13:52:09 -0700

    Updated code to reflect a single dictionary

--- GSE/telemetry_processor.py
+++ GSE/telemetry_processor.py
@@ -1,18 +1,11 @@
-import csv
-import glob
-
 class TelemetryProcessor:
-    
-    #Global variable defining all data over time
-    data_dict = []
     
     """
     Loads in any calibration files or csv map (of the order of values in input list)
     """
     def __init__(packetMap):
-        #Selects csv file with unknown file name
-        packet = glob.glob('*.csv')
-        return packet
+        with open(packet) as data:
+        return data
         
     """
     Adds limit status to each parameter in dictionary
@@ -45,25 +38,20 @@ class TelemetryProcessor:
     {"second_data_set", calibrated value), ....}]
     """
     def processPacket(packet):
-        #Reads csv and places into a dictionary
-        with open(packet, newline = '') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                row['Time_Start'],row['Flight State'],row['exp time'],row['Altitude'], \
-                row['Velocity_X'],row['Velocity_Y'],row['Velocity_Z'],row['Attitude_X'],row['Attitude_Y'],row['Attitude_Z'], \
-                row['Acceleration_X'],row['Acceleration_Y'],row['Acceleration_Z'],row['Ang_Vel_X'],row['Ang_Vel_Y'],row['Ang_Vel_Z'], \
-                row['Liftoff Warning'],row['RCS Warning'],row['Escape Warning'],row['Chute Warning'],row['Landing Warning'],row['Fault Warning'], \
-                row['Frequency Measure'],row['Magnetometer'],row['Current'],row['Voltage'],row['Timer'], \
-                row['BD_Acceleration_X'],row['BD_Acceleration_Y'],row['BD_Acceleration_Z'], \
-                row['BD_Ang_Vel_X'],row['BD_Ang_Vel_Y'],row['BD_Ang_Vel_Z'],row['MOSFET'],row['Time_End']
-            #Note: Add Blue Dawn Errors to dictionary if they become part of csv
-            '''
-            Last 3 rows report Blue Dawn data. Order may need to be changed depending on final CSV data order. Written this
-            way so that a list of dictionaries is created. Each dictionary represents all data at a given point in time
-            '''
-            #Question: How often is the CSV updated? This model assumes all data at a given time is reported in one row of the CSV
-                data_dict.append(row)
+        #Reads data and places into a dictionary
+        data = packet.split(',')
+        data_dict = {'Time_Start' : data[0], 'Flight State' : data[1], 'exp time'] : data[2], 'Altitude' : data[3], \
+            'Velocity_X' : data[4], 'Velocity_Y' : data[5], 'Velocity_Z' : data[6], 'Attitude_X' : data[7], \
+            'Attitude_Y' : data[8], 'Attitude_Z' : data[9], 'Acceleration_X' : data[10], 'Acceleration_Y' : data[11], \
+            'Acceleration_Z' : data[12], 'Ang_Vel_X' : data[13], 'Ang_Vel_Y' : data[14], 'Ang_Vel_Z' : data[15], \
+            'Liftoff Warning' : data[16], 'RCS Warning' : data[17], 'Escape Warning' : data[18], 'Chute Warning' : data[19], \
+            'Landing Warning' : data[20], 'Fault Warning' : data[21], 'Frequency Measure' : data[22], 'Magnetometer' : data[23], \
+            'Current' : data[24], 'Voltage' : data[25], 'Timer' : data[26], \
+            'BD_Acceleration_X' : data[27], 'BD_Acceleration_Y' : data[28], 'BD_Acceleration_Z' : data[29], \
+            'BD_Ang_Vel_X' : data[30], 'BD_Ang_Vel_Y' : data[31], 'BD_Ang_Vel_Z' : data[32], 'MOSFET' : data[33], 'Time_End' : data[34]}
+        #Note: Add Blue Dawn Errors to dictionary if they become part of csv
+        #Question: How often is the data updated? 
                 
-            data_dict = addLimitStatus(data_dict)
-            return data_dict
+        data_dict = addLimitStatus(data_dict)
+        return data_dict
            
